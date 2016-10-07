89da8eae0e0a6d08f3a29f99571e46c428e48fe8
xzzpig <xzzpig@vip28>
2016-10-07 12:36:09 +0800

    rebuild

--- src/com/github/xzzpig/example/Main.java
+++ src/com/github/xzzpig/example/Main.java
@@ -1,8 +1,15 @@
 package com.github.xzzpig.example;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.xzzpig.pigapi.PigData;
+import com.github.xzzpig.pigapi.bukkit.TCommandHelp;
+
 public class Main extends JavaPlugin {
+	public static PigData data;
+	
 	@Override
 	public void onEnable() {
 		getLogger().info(getName() + getDescription().getVersion() + "插件已被加载");
@@ -13,6 +20,23 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		getLogger().info(getName() + "插件已被停用 ");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		String arg0 = "help";
+		try {
+			arg0 = args[0];
+		} catch (Exception e) {}
+		if (arg0.equalsIgnoreCase("help")) {
+			if (arg0.equalsIgnoreCase("help")) {
+				for(TCommandHelp sub:Help.Example.getSubCommandHelps()){
+					sub.getHelpMessage("ExpProtect").send(sender);
+				}
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
