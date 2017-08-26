9e93a6511e158801299416b8d60c8b33ea206c4d
xzzpig <1146628972@qq.com>
2017-08-26 14:44:54 +0800

    添加onTabComplete

--- src/com/github/xzzpig/example/Main.java
+++ src/com/github/xzzpig/example/Main.java
@@ -1,5 +1,7 @@
 package com.github.xzzpig.example;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,5 +23,10 @@ public class Main extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		return Help.Example.runCommand(Help.Example.new CommandInstance(sender, command, label, args));
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		return Help.Example.getTabComplete(getName(), sender, command, alias, args);
 	}
 }
